236a07ea63c87fb28749a71edf9523d8903a3b6d
ryandowler-commits <ryan.dowler@gmail.com>
2026-03-20 11:15:59 -0400

    adding cart slice functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,33 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const newItem = state[action.payload];
+      const existsingItem = state.items.find(item => item.id === newItem.id);
+      if(!existsingItem){ 
+        //item does not exist, add to cart with quantity 1
+        const newItemWithQty = { ...newItem, quantity: 1 };
+        const updatedItems = state.items.push(newItemWithQty);
+      } else {
+        //otherwise, item exists, increment quantity
+        const updatedItem = { ...existsingItem, quantity: existsingItem.quantity += 1 };
+        const updatedItems = state.items.map(item => item.id === newItem.id ? updatedItem : item);  
+
+      };
     },
     removeItem: (state, action) => {
+      const item = state[action.payload];
+      const updatedItems = state.items.splice(state.items.indexOf(item), 1);
     },
     updateQuantity: (state, action) => {
-
-    
+      const itemToUpdate = state[action.payload] ;
+      const newQuantity = itemToUpdate.quantity ;
+      if (newQuantity > 0) {
+        const updatedItems = state.items.map(item => item.id === itemToUpdate.id ? itemToUpdate : item);  
+  
+    } else {
+      //remove item if quantity is zero or less
+        const updatedItems = state.items.splice(state.items.indexOf(itemToUpdate), 1);
+      };
     },
   },
 });
