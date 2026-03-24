9cf57f13881dd7a4b2cd6eb46c20bc41cf27f7e1
ryandowler-commits <ryan.dowler@gmail.com>
2026-03-24 09:42:59 -0400

    correcting dispatch call

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
